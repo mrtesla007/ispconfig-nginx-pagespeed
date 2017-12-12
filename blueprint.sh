5e133bfcf46ebafeef0bc1037dacc69838b5175c
speedwordpress <dani@speedword.press>
2017-12-12 16:57:15 +0100

    Update blueprint.sh

--- blueprint.sh
+++ blueprint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+echo "Paketliste wird ergänzt"
 # export DEBIAN_FRONTEND="noninteractive"
+wget https://www.dotdeb.org/dotdeb.gpg
+apt-key add dotdeb.gpg
+echo "deb http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list
+echo "deb-src http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list
+apt-get update
