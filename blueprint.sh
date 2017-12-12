58e88fc948fcbb8ce9b9425493f8a67f1ebc46a1
speedwordpress <dani@speedword.press>
2017-12-12 17:15:41 +0100

    Update blueprint.sh

--- blueprint.sh
+++ blueprint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-echo "Paketliste wird ergänzt"
-# export DEBIAN_FRONTEND="noninteractive"
+echo -n "Paketliste wird ergänzt"
+export DEBIAN_FRONTEND="noninteractive"
 wget https://www.dotdeb.org/dotdeb.gpg
 apt-key add dotdeb.gpg
 echo "deb http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list
 echo "deb-src http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list
-apt-get update
+echo -n "Vollständiges Updaten... "
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
+echo -n "Must have Software... "
+apt-get -y install ssh openssh-server sudo php5-cli python-software-properties ufw ntp ntpdate debconf-utils git lsb-release haveged e2fsprogs jessie-backports libssl1.0.0
+echo -n "Konfiguriere Dash... "
+dpkg-reconfigure -f noninteractive dash
+echo -n "Erstelle Unix Benutzer mit Sudo Rechten... "
+# adduser isp && usermod -a -G sudo isp
+
+
