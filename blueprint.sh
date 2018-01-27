89e9614d3b942843cd6b5ca20917cbb137f9ea62
speedwordpress <dani@speedword.press>
2018-01-27 18:27:30 +0100

    Update blueprint.sh

--- blueprint.sh
+++ blueprint.sh
@@ -11,7 +11,8 @@ echo -n "Must have Software... "
 apt-get -y install ssh openssh-server sudo php5-cli python-software-properties ufw ntp ntpdate debconf-utils git lsb-release haveged e2fsprogs jessie-backports libssl1.0.0
 echo -n "Konfiguriere Dash... "
 dpkg-reconfigure -f noninteractive dash
-echo -n "Erstelle Unix Benutzer mit Sudo Rechten... "
+
+# echo -n "Erstelle Unix Benutzer mit Sudo Rechten... "
 # adduser isp && usermod -a -G sudo isp
 
 
